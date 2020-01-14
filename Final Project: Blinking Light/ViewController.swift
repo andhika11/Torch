d5aaa6b5866d4cf04e835018298e68d9014b1261
Bonny <andhikatirtawisata@andhikas-macbook-air.local>
2020-01-14 14:52:20 +0700

    Initial Commit

---
+++ Final Project: Blinking Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final Project: Blinking Light
+//
+//  Created by Andhika Tirtawisata on 14/01/20.
+//  Copyright © 2020 Andhika Tirtawisata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
