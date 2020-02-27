3ea37f6088749d9147b5f90b81594f2cedde6ca5
Andhika Tirtawisata <andhikatirtawisata@Andhikas-MacBook-Air.local>
2020-02-27 14:55:41 +0700

    First commit

--- Final Project: Blinking Light/ViewController.swift
+++ Final Project: Blinking Light/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+    
+    @IBOutlet weak var lightButton: UIButton!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        lightOn = !lightOn
+        
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
+        } else {
+            view.backgroundColor = .black
+            lightButton.setTitle("On", for: .normal)
+        }
+    }
+    
+    func toggleTorch() {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
